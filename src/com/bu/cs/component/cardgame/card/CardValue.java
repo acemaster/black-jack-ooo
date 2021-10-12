b044aecb06c75f5b12fa161259cd7fd33e7d1939
acemaster <vivekhtc25@gmail.com>
2021-10-12 17:03:01 -0400

    Added alt Value to Card value class

--- src/com/bu/cs/component/cardgame/card/CardValue.java
+++ src/com/bu/cs/component/cardgame/card/CardValue.java
@@ -2,7 +2,7 @@ package com.bu.cs.component.cardgame.card;
 
 public enum CardValue {
 
-    ACE(1),
+    ACE(1,11),
     TWO(2),
     THREE(3),
     FOUR(4),
@@ -17,12 +17,23 @@ public enum CardValue {
     KING(10);
 
     private int value;
+    private int altValue;
 
     CardValue(int value) {
         this.value = value;
+        this.altValue = 0;
+    }
+
+    CardValue(int value,int altValue) {
+        this.value = value;
+        this.altValue = altValue;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public int getAltValue() {
+        return altValue;
     }
 }
