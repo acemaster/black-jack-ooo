04ab8686542db9c4a1d796645306ddd2f3687fea
acemaster <vivekhtc25@gmail.com>
2021-10-12 11:26:31 -0400

    Added skeletons for Dealer and Trianta player

---
+++ src/com/bu/cs/component/cardgame/Dealer.java
@@ -0,0 +1,4 @@
+package com.bu.cs.component.cardgame;
+
+public class Dealer extends CardPlayer{
+}
---
+++ src/com/bu/cs/component/cardgame/trianta/TriantaPlayer.java
@@ -0,0 +1,6 @@
+package com.bu.cs.component.cardgame.trianta;
+
+import com.bu.cs.component.cardgame.CardPlayer;
+
+public class TriantaPlayer extends CardPlayer {
+}
