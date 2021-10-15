80b814504adb1d25be23cc84956b75640ca89b18
acemaster <vivekhtc25@gmail.com>
2021-10-15 00:35:35 -0400

    Fixed Card compare logic

--- src/com/bu/cs/component/cardgame/card/Card.java
+++ src/com/bu/cs/component/cardgame/card/Card.java
@@ -51,10 +51,7 @@ public class Card implements Comparable<Card>{
 
 	@Override
 	public int compareTo(Card o) {
-		if(cardValue.equals(o.getCardValue()))
-			return 1;
-		else
-			return 0;
+		return this.cardValue.getValue() - o.getCardValue().getValue();
 	}
 
 
